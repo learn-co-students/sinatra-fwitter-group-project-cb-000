b667bf150a996a4c108af95cfa165635cb04a5fd
Jordan Sleeper <ezirens@gmail.com>
2017-01-06 18:15:38 +0000

    user#slug and .find_by_slug

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
 
+  #user needs slug method to name-stuff
+  def slug
+    @username.downcase.gsub(' ', '-')
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{|i| i.slug == slug}
+  end
+
 end
