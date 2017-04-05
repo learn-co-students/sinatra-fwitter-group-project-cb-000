357358e6c292f0df87f937b8466cb88fbcbe62ec
John Knotts <jhnnyk@gmail.com>
2017-04-05 15:36:15 -0600

    user signup, login & logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,81 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_secret"
+  end
+
+  helpers do
+
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      @user = User.find_by(id: session[:user_id])
+    end
+
   end
 
   get '/' do
     erb :index
+  end
+
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/signup' do
+    @user = User.new(username: params[:username],
+                        email: params[:email],
+                     password: params[:password])
+
+    if @user.save
+      # TODO: add flash message for successful signup and login
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    else
+      # TODO: add flash message for unsucessful signup
+      redirect to '/signup'
+    end
+  end
+
+  get '/tweets' do
+    if logged_in?
+      erb :'tweets/tweets'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :'users/login'
+    else
+      redirect to '/tweets'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+
+    if @user && @user.authenticate(params[:password])
+      # TODO: add flash message for successful login
+      session[:user_id] = @user.id
+      redirect to '/tweets'
+    else
+      # TODO: add flash message for unsucessful login
+      redirect to '/login'
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect to '/login'
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+  validates_presence_of :username
+  validates_presence_of :email
+  validates_presence_of :password
 end
