561fed6ade909bc1879b9fabb1670a744900cd35
tahbristol <tahbristol@gmail.com>
2017-06-05 02:07:06 -0400

    tried passing no logged user see signup page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < Sinatra::Base
   register Sinatra::Reloader
 
    configure do
-     enable :session
+     enable :sessions
      set :session_secret, "my_application_secret"
   set :public_folder , Proc.new {File.join(root,"../public")}
     set :views, 'app/views'
@@ -17,22 +17,43 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/tweets'do
+    erb :'/tweets/index'
+  end
 
   get '/signup'do
-      if logged_in?
-        redirect to "/users/tweets"
+      if logged_in?(session)
+        puts "HELLO LOGGER"
+        redirect to "/tweets"
       else
-
-    erb :'/users/new'
+        puts "EEEEEEEEEEEEEEEEEEEEE"
+    erb :'/users/create_user'
   end
+  end
+
+  get '/login'do
+
+     erb :'/users/login'
+  end
+##########################################################################
+  post '/login'do
+
+     @user = User.find_by(username: params[:username])
+     if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect to "/tweets"
+     else
+       redirect to "/login"
+
+     end
   end
 
   post '/signup'do
 
      if !params[:username].empty? && !params[:email].empty? && !params[:password].empty?
 
-
-redirect to "/users/tweets"
+        @user = User.create(username: params[:username], email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
+       redirect to "/tweets"
      else
        redirect to "/signup"
      end
@@ -43,7 +64,7 @@ redirect to "/users/tweets"
 
   helpers do
 
-    def logged_in?
+    def logged_in?(session)
         !!session[:id]
     end
 
