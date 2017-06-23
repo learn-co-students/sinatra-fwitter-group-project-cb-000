f3109aaf1547c577aa9fd62da2acf172fdaf74c0
DV <github email address>
2017-06-23 23:04:53 +0900

    completed lesson

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,20 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
+  use Rack::Flash
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "sooper-dooper-secret-930022"
+  end
+
+  get '/' do
+    if !!session[:user_id]
+      redirect '/tweets'
+    else
+      erb :index
+    end
   end
 
 end
