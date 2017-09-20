ff0ac06184dbee9d0cc1966fcdf708f14101dd92
theboymo <billfero@gmail.com>
2017-09-20 15:44:45 +0100

    impl a signup form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,6 +18,44 @@ class ApplicationController < Sinatra::Base
     erb :'/users/login'
   end
 
+  post '/login' do
+    # TODO
+    # - authenticate user
+    # - create session
+  end
+
+  delete '/logout' do
+    # TODO - destroy session
+  end
+
+  get '/signup' do
+    erb :'/users/create_user'
+  end
+
+  post '/signup' do
+    # {
+    #   "username"=>"joe blogs",
+    #   "email"=>"joe@example.com",
+    #   "password"=>"password"
+    # }
+    
+    # TODO
+    # create user
+
+    # log them in
+
+    # redirect to tweets index
+    redirect :'/tweets'
+  end
+
+  get '/tweets' do
+    # check user is logged in
+
+    # if so display all tweets
+    erb :'/tweets/tweets'
+  end
+
+
 
   helpers do
 
