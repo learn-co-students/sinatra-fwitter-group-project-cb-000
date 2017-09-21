8a43526d38e76f0e1efc6a5a372ea60c8042152b
theboymo <billfero@gmail.com>
2017-09-21 20:25:35 +0100

    impl routes to create a tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,5 +9,28 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/create_tweet'
+    else
+      redirect :'/login'
+    end
+  end
+
+  post '/tweets' do
+    if logged_in?
+      if params[:content] && params[:content] != ''
+        @tweet = Tweet.new(content: params[:content])
+        @tweet.user = current_user
+        @tweet.save
+        erb :'/tweets/show_tweet'
+      else
+        # TODO add flash error message
+        redirect :'/tweets/new'
+      end
+    else
+      redirect :'/login'
+    end
+  end
 
 end
