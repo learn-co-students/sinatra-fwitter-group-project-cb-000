f8fa614747c55d438a1b38b52c728b4c92bfab62
Michael Wahlin <wahlin@gmail.com>
2018-01-02 21:10:41 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,31 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+
+    enable :sessions
+    set :session_secret, "secret"
   end
 
+  get '/' do
+    erb :index
+  end
+
+  get '/signup' do
+    if session["user_id"]
+      redirect '/tweets/tweets'
+    end
+
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    if params["username"].empty? || params["email"].empty? || params["password"].empty?
+      redirect '/signup'
+    end
+
+    @user = User.create(params)
+    session["user_id"] = @user.id
+
+    redirect 'tweets/tweets'
+  end
 end
