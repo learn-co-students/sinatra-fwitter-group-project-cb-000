824417a1686d7674d590379b3a2e3e99ecc2c578
adam n <adam.nawroc@gmail.com>
2018-04-03 16:24:39 +0000

    more sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,38 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  enable :sessions
 
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    set :session_secret, "secret"
+
   end
 
   get '/' do
     erb :index
   end
 
+  get '/tweets' do
+    erb :tweets
+  end
+
   get '/signup' do
 
     # not quite sure what it means
-    erb :signup
-    # redirect '/tweets'
+    # erb :signup
+
+    if logged_in?
+    #  erb :new
+     redirect '/tweets'
+
+   else
+     erb :signup
+     # erb :error
+    #  redirect '/error'
+
+   end
   end
 
   post '/signup' do
@@ -30,6 +47,16 @@ class ApplicationController < Sinatra::Base
     end
     # erb :signup
     # redirect '/tweets'
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    # def current_user
+    #   User.find(session[:user_id])
+    # end
   end
 
 end
