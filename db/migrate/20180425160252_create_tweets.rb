d3ace3132a4cad198fbd3230aa109c1356333620
Chris Cardle <cdcardle@gmail.com>
2018-04-25 16:15:12 +0000

    models and migrations setup

--- db/migrate/20180425160252_create_tweets.rb
+++ db/migrate/20180425160252_create_tweets.rb
@@ -1,8 +1,8 @@
 class CreateTweets < ActiveRecord::Migration
   def change
     create_table :tweets do |t|
-      t.string :title
-
+      t.string :content
+      t.integer :user_id
     end
   end
 end
