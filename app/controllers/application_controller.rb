237ef4b708bf482800c8ccfc269ad3846ab51e49
Chris Cardle <cdcardle@gmail.com>
2018-04-28 01:42:51 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,62 +9,115 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "not telling"
   end
 
+  # Homepage
+
   get '/' do
     erb :index
   end
 
+  # Sign Up
+
   get '/signup' do
-    erb :signup
+    if logged_in?
+      redirect "/#{current_user.slug}/tweets"
+    else
+      erb :signup
+    end
   end
 
   post '/signup' do
     if params[:username].empty? || params[:email].empty? || params[:password].empty?
-      raise StandardError, "All boxes must be filled out!"
+      redirect "/error"
     else
-      @user = User.create(params)
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      if @user.save
+        session[:user_id] = @user.id
+        redirect "/#{current_user.slug}/tweets"
+      else
+        redirect "/error"
+      end
     end
-    session[:id] = @user.id
-
-    redirect "/<%= current_user.slug %>/tweets"
   end
+
+  # Log In
 
   get '/login' do
     if logged_in?
-      redirect "/<%= current_user.slug %>/tweets"
+      redirect #{current_user.slug}weets"
     else
       erb :login
     end
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username], password: params[:password])
-    session[:id] = @user.id
-
-    # redirect "/<%= current_user.slug %>/tweets"
-    erb :tweets
+    puts params
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/#{current_user.slug}/tweets"
+    else
+      redirect 'error'
+    end
   end
 
+  # Log Out
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+    end
+
+    redirect '/'
+  end
+
+  # Users
+
   get '/:slug' do
-    redirect "/<%= current_user.slug %>/tweets"
+    @user = User.find_by_slug(params[:slug])
+
+    redirect "/#{current_user.slug}/tweets"
   end
 
   get '/:slug/tweets' do
+    @user = User.find_by_slug(params[:slug])
+    erb :tweets
+  end
+
+  # Tweets
+
+  get '/:slug/tweets/new' do
     if logged_in?
-      @user = User.find_by_slug(params[:slug])
-      erb :tweets
+      erb :new
     else
       redirect '/login'
     end
   end
 
-  get '/logout' do
-    session.clear
-    redirect '/'
+  get '/:slug/tweets/:id' do
+    @user = User.find_by_slug(params[:slug])
+    @tweet = Tweet.find(params[:id])
+    erb :show
+  end\
+
+  get '/:slug/tweets/:id/edit' do
+    if current_user
+      erb :edit
+    else
+      redirect "/error"
+    end
   end
 
+  # Errors
+
+  get '/error' do
+    erb :errors
+  end
+
+  # Helpers
+
   helpers do
-    def current_user(session_hash)
-      User.find(session_hash[:user_id])
+    def current_user
+      User.find(session[:user_id])
     end
 
     def logged_in?
