9de33309e76ea19db4320d6e00fdd82fd3ad465f
Alex Segers <a.segers.dev@gmail.com>
2018-05-31 17:34:49 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,22 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do #homepage
+    erb :'application/root'
+  end
+
+  get '/tweets' do
+    @tweets = Tweet.all
+    erb :'tweets/index'
+  end
+
+  get '/tweets/new' do
+    erb :'tweets/new'
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find(params[:id])
+    erb :'tweets/show'
+  end
+
 end
