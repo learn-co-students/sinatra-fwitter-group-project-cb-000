0e3979303b6a9582f9d27c0cd46d6dc3bb91534b
Alex Segers <a.segers.dev@gmail.com>
2018-06-08 22:12:31 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
+  use Rack::Flash
 
   enable :sessions
   set :session_secret, "secret"
@@ -8,6 +10,16 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+  end
+
+  helpers do
+    def current_user
+      User.find_by(:password_digest = session[:digest])
+    end
+
+    def logged_in?
+      session.key?(:digest)
+    end
   end
 
 end
