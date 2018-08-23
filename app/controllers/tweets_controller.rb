369729ba4122b605a71c84ed244203f6253217cf
Kirby Campbell <jkirbycampbell@gmail.com>
2018-08-23 18:34:55 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,16 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    erb :'/tweets/tweets'
+  end
+
+  get '/tweets/new' do
+    erb :'/tweets/create_tweet'
+  end
+
+  post '/tweets' do
+    erb :'/show_tweet'
+  end
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
 
+  get '/users' do
+    erb :'/users/show'
+  end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  get '/signup' do
+    if !logged_in?
+      erb :'/users/create_user'
+    else
+      redirect "/tweets"
+    end
+  end
+
+  post '/signup' do
+    if params[:username] != "" && params[:email] != "" && params[:password] != ""
+      @user = User.create(:email => params[:email], :username => params[:username], :password => params[:password])
+      if @user.save
+        session[:user_id] = @user.id
+        redirect "/tweets"
+      else
+         redirect "/signup"
+       end
+    else
+      redirect "/signup"
+    end
+  end
 
 end
