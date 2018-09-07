97fad938ec2f4db6431333cafd71df45de223141
Adam Urbina <adamurbina@gmail.com>
2018-09-07 14:28:01 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,30 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    erb :'/layout'
+  end
+
+  get '/login' do
+    erb :'/login'
+  end
+
+  get '/signup' do
+    erb :'/signup'
+  end
+
+  post '/signup' do
+    # params.each do |key, value|
+    #   redirect '/signup' if value.empty?
+    # end
+
+    params.each do |key, value|
+      value.empty? ? (redirect '/signup') : (key = value)
+    end
+
+    binding.pry
+    erb :'/twitter/index'
+  end
 
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :tweets
+
+  def slug
+    @username = self.username
+    @username.gsub " ", "-"
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find do |user|
+       if user.slug == slug
+         return user
+       end
+    end
+  end
+
 end
