ffa01e4923587ed1698ee1171f4d6ca9494dcaf5
Sergio Marrocoli <s.marrocoli@gmail.com>
2018-12-28 16:43:05 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
   end
 
   get '/' do
@@ -16,20 +17,32 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/signup' do
-    # binding.pry
-    if((params[:username] == "" || params[:email] == "") || params[:password] = "")
+
+    if((params[:username] == "" || params[:email] == "") || params[:password] == "")
       redirect :signup
     else
       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
-      redirect :'/tweets'
+      session[:user_id] = @user.id
+      redirect :'/'
     end
   end
 
   get '/login' do
-    if sessions[:user_id] == nil
-      erb :'users/login'
+    if session[:user_id] == nil
+      redirect '/login'
     else
       redirect '/'
+    end
+  end
+
+  post '/login' do
+    # binding.pry
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] == @user.id
+      redirect '/'
+    else
+      redirect '/login'
     end
   end
 
