6623f05224ae52fdef39c18053fd9ae866dcb8f2
Ali Alsayed <a_alsayed87@hotmail.com>
2019-03-05 11:16:30 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,75 @@
 class TweetsController < ApplicationController
+  get '/tweets' do
+    if Helpers.logged_in?(session)
+      @tweets = Tweet.all
+      erb :'tweets/tweets'
+    else
+      redirect '/login'
+    end
+  end
 
+  get '/tweets/new' do
+    if Helpers.logged_in?(session)
+      erb :'tweets/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/tweets' do
+    if Helpers.logged_in?(session)
+      if !params[:content].empty?
+        Helpers.current_user(session).tweets << Tweet.create(content: params[:content])
+      else
+        redirect '/tweets/new'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id' do
+    if Helpers.logged_in?(session)
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
+    if Helpers.logged_in?(session)
+      @tweet = Tweet.find(params[:id])
+      erb :'tweets/edit'
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/tweets/:id' do
+    if Helpers.logged_in?(session)
+      @tweet = Tweet.find(params[:id])
+      if !params[:content].empty?
+        @tweet.update(content: params[:content])
+        redirect "/tweets/#{@tweet.id}"
+      else
+        redirect "/tweets/#{@tweet.id}/edit"
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  delete '/tweets/:id/delete' do
+    if Helpers.logged_in?(session)
+      @tweet = Tweet.find(params[:id])
+      if @tweet.user == Helpers.current_user(session)
+        @tweet.delete
+        redirect '/tweets'
+      end
+    else
+      redirect '/login'
+    end
+  end
 
 end
